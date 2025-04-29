771d0546510398e245cbc17d7efc127fabbda07c
EngincanV <enginveske@gmail.com>
2025-04-29 09:05:12 +0300

    create app services

---
+++ src/AbpCustomTenantLogo.Application.Contracts/ICustomTenantLogoAppService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace AbpCustomTenantLogo
+{
+    public interface ICustomTenantLogoAppService : IApplicationService
+    {
+        Task UpdateTenantLogoUrlAsync(Guid? tenantId, string tenantLogoUrl);
+    }
+}
---
+++ src/AbpCustomTenantLogo.Application/CustomTenantLogoAppService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Application.Services;
+using Volo.Abp.SettingManagement;
+
+namespace AbpCustomTenantLogo
+{
+    public class CustomTenantLogoAppService : ApplicationService, ICustomTenantLogoAppService
+    {
+        private readonly ISettingManager _settingManager;
+
+        public CustomTenantLogoAppService(ISettingManager settingManager)
+        {
+            _settingManager = settingManager;
+        }
+        public async Task UpdateTenantLogoUrlAsync(Guid? tenantId, string tenantLogoUrl)
+        {
+            //not host tenant, and if the tenantId is not the current tenant id, throw an exception
+            if(CurrentTenant.Id.HasValue && tenantId != CurrentTenant.Id.Value)
+            {
+                throw new UserFriendlyException("You are not allowed to update this tenant's logo.");
+            }
+
+            await _settingManager.SetForCurrentTenantAsync("CustomTenantLogo", tenantLogoUrl);
+        }
+    }
+}
