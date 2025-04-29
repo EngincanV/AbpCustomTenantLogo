37c0aae6808d11ad37fd86be7dd18f340f7aaba6
EngincanV <enginveske@gmail.com>
2025-04-29 09:06:50 +0300

    create view component

---
+++ src/AbpCustomTenantLogo.Web/Components/CustomTenantLogoViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.SettingManagement;
+
+namespace AbpCustomTenantLogo.Web.Components
+{
+    public class CustomTenantLogoViewComponent : AbpViewComponent
+    {
+        private readonly ISettingManager _settingManager;
+
+        public CustomTenantLogoViewComponent(ISettingManager settingManager)
+        {
+            _settingManager = settingManager;
+        }
+
+        public virtual async Task<IViewComponentResult> InvokeAsync()
+        {
+            var customTenantLogoSetting = await _settingManager.GetOrNullForCurrentTenantAsync("CustomTenantLogo");
+
+            return View("~/Components/Default.cshtml", new CustomTenantLogoViewModel
+            {
+                CustomTenantLogoUrl = customTenantLogoSetting
+            });
+        }
+    }
+
+    public class CustomTenantLogoViewModel
+    {
+        public string CustomTenantLogoUrl { get; set; }
+    }
+}
