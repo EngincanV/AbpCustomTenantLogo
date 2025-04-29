9f5b737da8d254143de903e19b915527f282f2ef
EngincanV <enginveske@gmail.com>
2025-04-29 09:08:45 +0300

    Update AbpCustomTenantLogoBrandingProvider.cs

--- src/AbpCustomTenantLogo.Web/AbpCustomTenantLogoBrandingProvider.cs
+++ src/AbpCustomTenantLogo.Web/AbpCustomTenantLogoBrandingProvider.cs
@@ -1,19 +1,36 @@
-﻿using Volo.Abp.Ui.Branding;
+﻿using System.Threading.Tasks;
+using Volo.Abp.Ui.Branding;
 using Volo.Abp.DependencyInjection;
-using Microsoft.Extensions.Localization;
-using AbpCustomTenantLogo.Localization;
+using Volo.Abp.MultiTenancy;
+using Volo.Abp.SettingManagement;
+using Volo.Abp.Threading;
 
 namespace AbpCustomTenantLogo.Web;
 
 [Dependency(ReplaceServices = true)]
 public class AbpCustomTenantLogoBrandingProvider : DefaultBrandingProvider
 {
-    private IStringLocalizer<AbpCustomTenantLogoResource> _localizer;
+    private readonly ISettingManager _settingManager;
+    private readonly ICurrentTenant _currentTenant;
 
-    public AbpCustomTenantLogoBrandingProvider(IStringLocalizer<AbpCustomTenantLogoResource> localizer)
+    public AbpCustomTenantLogoBrandingProvider(ISettingManager settingManager, ICurrentTenant currentTenant)
     {
-        _localizer = localizer;
+        _settingManager = settingManager;
+        _currentTenant = currentTenant;
     }
 
-    public override string AppName => _localizer["AppName"];
+    public override string? LogoUrl => AsyncHelper.RunSync(GetTenantLogoUrlAsync) ?? base.LogoUrl;
+
+    public async Task<string?> GetTenantLogoUrlAsync()
+    {
+        //add caching and other logic here!!!
+        var tenantId = _currentTenant.Id;
+        if(!tenantId.HasValue)
+        {
+            return null;
+        }
+
+        var customTenantLogoUrl = await _settingManager.GetOrNullForCurrentTenantAsync("CustomTenantLogo");
+        return string.IsNullOrWhiteSpace(customTenantLogoUrl) ? null : customTenantLogoUrl;
+    }
 }
